8e1d60434ed270ca78526de2fd12b5277ad78479
open-meadow <rohan.varughese@protonmail.com>
2023-02-03 15:42:46 -0500

    Initial commit

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -17,14 +17,49 @@ export default function App() {
       });
   }, []);
 
- 
+  const opts = {
+    playerVars: {
+      rel: 0,
+    },
+  };
+
+  const invidiousEndpoint = "https://invidio.us/api/v1/videos/";
+  const videoId = "MWQkvbe5nyY";
+
+  const getVideoSource = async function (videoId) {
+    const response = await fetch(invidiousEndpoint + videoId);
+    console.log("response: ", response);
+    const data = await response.json();
+    return data.files[0].url;
+  };
+
+  const VideoPlayer = ({ videoId }) => {
+    const [source, setSource] = useState("");
+
+    useEffect(() => {
+      getVideoSource(videoId).then((source) => {
+        setSource(source);
+      });
+    }, [videoId]);
+
+    return <video controls={true} src={source} style={{ width: "100%" }} />;
+  };
 
   return (
     <div className="App">
       <h1>Hello React World</h1>
-      <YouTube
-        videoId = "MWQkvbe5nyY"
-      />
+      <YouTube videoId="MWQkvbe5nyY" opts={opts} />
+
+      <VideoPlayer videoId={videoId} />
+
+      <iframe
+        src="https://www.youtube.com/embed/hy9Cbp_yV5Y?rel=0"
+        width="100%"
+        height="100%"
+        frameborder="0"
+      ></iframe>
+
+      <video></video>
 
       <section>
         {!status.error && (
