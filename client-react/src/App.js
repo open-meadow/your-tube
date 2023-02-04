7872cd5dc78c7179c6748e29b161c4590c1ae0f7
open-meadow <rohan.varughese@protonmail.com>
2023-02-03 19:05:16 -0500

    Safety commit before installing material UI

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -23,36 +23,83 @@ export default function App() {
     },
   };
 
-  const invidiousEndpoint = "https://invidio.us/api/v1/videos/";
-  const proxyEndpoint = "https://your-lambda-function.com/getVideoSource";
-  const videoId = "MWQkvbe5nyY";
+  // const invidiousEndpoint = "https://invidio.us/api/v1/videos/";
+  // const videoId = "MWQkvbe5nyY";
+  // const proxyEndpoint = "https://your-lambda-function.com/getVideoSource";
 
-  const getVideoSource = async function(videoId) {
-    const response = await fetch(`${proxyEndpoint}?videoId=${videoId}`);
-    console.log("response: ", response);
-    const data = await response.json();
-    return data.source;
-  };
+  // const getVideoSource = async function(videoId) {
+  //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  //   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
+  //   const targetUrl = proxyUrl + invidiousEndpoint;
+  
+  //   try {
+  //     const response = await fetch(targetUrl);
+  //     const data = await response.json();
+  //     return data.files[0].url;
+  //   } catch (error) {
+  //     console.error(error);
+  //     return null;
+  //   }
+  // };
 
+  // exports.handler = async (event, context) => {
+  //   const videoId = event.queryStringParameters.videoId;
+  //   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
 
-  const VideoPlayer = ({ videoId }) => {
-    const [source, setSource] = useState("");
+  //   try {
+  //     const response = await axios.get(invidiousEndpoint);
+  //     return {
+  //       statusCode: 200,
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "*",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         source: response.data.files[0].url,
+  //       }),
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       statusCode: 500,
+  //       headers: {
+  //         "Access-Control-Allow-Origin": "*",
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({
+  //         error: error.message,
+  //       }),
+  //     };
+  //   }
+  // };
 
-    useEffect(() => {
-      getVideoSource(videoId).then((source) => {
-        setSource(source);
-      });
-    }, [videoId]);
-
-    return <video controls={true} src={source} style={{ width: "100%" }} />;
-  };
+  // const VideoPlayer = (props) => {
+  //   const [videoUrl, setVideoUrl] = useState(null);
+  
+  //   useEffect(() => {
+  //     const getVideo = async () => {
+  //       const videoSource = await getVideoSource(props.videoId);
+  //       setVideoUrl(videoSource);
+  //     };
+  
+  //     getVideo();
+  //   }, [props.videoId]);
+  
+  //   return (
+  //     <div>
+  //       {videoUrl ? <video src={videoUrl} controls /> : <p>Loading...</p>}
+  //     </div>
+  //   );
+  // };
 
   return (
-    <div className="App">
-      <h1>Hello React World</h1>
+    <main className="App">
+      <nav className="navbar">
+        <h1>Hello React World</h1>
+      </nav>
+
       <YouTube videoId="MWQkvbe5nyY" opts={opts} />
 
-      <VideoPlayer videoId={videoId} />
+      {/* <VideoPlayer videoId="MWQkvbe5nyY" /> */}
 
       <iframe
         src="https://www.youtube.com/embed/hy9Cbp_yV5Y?rel=0"
@@ -75,6 +122,6 @@ export default function App() {
           </>
         )}
       </section>
-    </div>
+    </main>
   );
 }
