a633b08f84e8443a2d20b5b71e8461a2afc82bd3
open-meadow <rohan.varughese@protonmail.com>
2023-02-03 20:38:46 -0500

    Initial styling

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,7 +1,21 @@
+import * as React from "react";
 import "App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import YouTube from "react-youtube";
+
+// import fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+// import Material UI components
+import { Box, AppBar, Toolbar } from "@mui/material";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import { flexbox } from "@mui/system";
 
 export default function App() {
   const [status, setStatus] = useState({});
@@ -23,92 +37,29 @@ export default function App() {
     },
   };
 
-  // const invidiousEndpoint = "https://invidio.us/api/v1/videos/";
-  // const videoId = "MWQkvbe5nyY";
-  // const proxyEndpoint = "https://your-lambda-function.com/getVideoSource";
-
-  // const getVideoSource = async function(videoId) {
-  //   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  //   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
-  //   const targetUrl = proxyUrl + invidiousEndpoint;
-  
-  //   try {
-  //     const response = await fetch(targetUrl);
-  //     const data = await response.json();
-  //     return data.files[0].url;
-  //   } catch (error) {
-  //     console.error(error);
-  //     return null;
-  //   }
-  // };
-
-  // exports.handler = async (event, context) => {
-  //   const videoId = event.queryStringParameters.videoId;
-  //   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
-
-  //   try {
-  //     const response = await axios.get(invidiousEndpoint);
-  //     return {
-  //       statusCode: 200,
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         source: response.data.files[0].url,
-  //       }),
-  //     };
-  //   } catch (error) {
-  //     return {
-  //       statusCode: 500,
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         error: error.message,
-  //       }),
-  //     };
-  //   }
-  // };
-
-  // const VideoPlayer = (props) => {
-  //   const [videoUrl, setVideoUrl] = useState(null);
-  
-  //   useEffect(() => {
-  //     const getVideo = async () => {
-  //       const videoSource = await getVideoSource(props.videoId);
-  //       setVideoUrl(videoSource);
-  //     };
-  
-  //     getVideo();
-  //   }, [props.videoId]);
-  
-  //   return (
-  //     <div>
-  //       {videoUrl ? <video src={videoUrl} controls /> : <p>Loading...</p>}
-  //     </div>
-  //   );
-  // };
-
   return (
     <main className="App">
-      <nav className="navbar">
-        <h1>Hello React World</h1>
-      </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar sx={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <h1>YourTube</h1>
+          <AccountCircle sx={{height: '25%'}}/>
+        </AppBar>
+      </Box>
+      <Toolbar>{/* content */}</Toolbar>
 
+      <h1>Hello React World</h1>
       <YouTube videoId="MWQkvbe5nyY" opts={opts} />
 
       {/* <VideoPlayer videoId="MWQkvbe5nyY" /> */}
 
-      <iframe
+      {/* <iframe
         src="https://www.youtube.com/embed/hy9Cbp_yV5Y?rel=0"
         width="100%"
         height="100%"
         frameborder="0"
-      ></iframe>
+        ></iframe>
 
-      <video></video>
+      <video></video> */}
 
       <section>
         {!status.error && (
@@ -125,3 +76,71 @@ export default function App() {
     </main>
   );
 }
+
+// const invidiousEndpoint = "https://invidio.us/api/v1/videos/";
+// const videoId = "MWQkvbe5nyY";
+// const proxyEndpoint = "https://your-lambda-function.com/getVideoSource";
+
+// const getVideoSource = async function(videoId) {
+//   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
+//   const targetUrl = proxyUrl + invidiousEndpoint;
+
+//   try {
+//     const response = await fetch(targetUrl);
+//     const data = await response.json();
+//     return data.files[0].url;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// };
+
+// exports.handler = async (event, context) => {
+//   const videoId = event.queryStringParameters.videoId;
+//   const invidiousEndpoint = `https://invidio.us/api/v1/videos/${videoId}`;
+
+//   try {
+//     const response = await axios.get(invidiousEndpoint);
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         source: response.data.files[0].url,
+//       }),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         error: error.message,
+//       }),
+//     };
+//   }
+// };
+
+// const VideoPlayer = (props) => {
+//   const [videoUrl, setVideoUrl] = useState(null);
+
+//   useEffect(() => {
+//     const getVideo = async () => {
+//       const videoSource = await getVideoSource(props.videoId);
+//       setVideoUrl(videoSource);
+//     };
+
+//     getVideo();
+//   }, [props.videoId]);
+
+//   return (
+//     <div>
+//       {videoUrl ? <video src={videoUrl} controls /> : <p>Loading...</p>}
+//     </div>
+//   );
+// };
--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
 import App from "App";
 import './index.css';
+
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+// root.render(<App />);
 
 ReactDOM.render(
   <React.StrictMode>
