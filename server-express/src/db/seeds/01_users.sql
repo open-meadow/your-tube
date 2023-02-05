d3612dca7c5d8999c3b4e1327c01a589ae78da76
Steve Willsmore <scwillsmore@gmail.com>
2023-02-05 12:32:07 -0500

    Add db schema and seed data.

---
+++ server-express/src/db/seeds/01_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username) VALUES
+('BigJim48');
---
+++ server-express/src/db/seeds/02_playlists.sql
@@ -0,0 +1,3 @@
+INSERT INTO playlists (name, description, user_id) VALUES
+('JavaScript', 'For learning the basics of JavaScript.', '1'),
+('Lofi Beats', 'Relaxing music to study to.', '1');
