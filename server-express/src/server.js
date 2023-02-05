75acaa958f4ccd4c4de8b77d4b4368f10a503d19
Steve Willsmore <scwillsmore@gmail.com>
2023-02-05 14:27:05 -0500

    Add test get route.

--- server-express/src/server.js
+++ server-express/src/server.js
@@ -1,27 +1,42 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const path = require('path');
+const db = require("../db/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // serve static files from ../build (needed for React)
 const cwd = process.cwd();
-const public = path.join(cwd, '..', 'public');
+const public = path.join(cwd, "..", "public");
 console.log("public dir: ", public);
 app.use(express.static(public));
 
 // Do Not make a route for "/" or it will override public
 
 app.get("/api/status", (req, res) => {
-  res.json({version: "1.01"});
+  res.json({ version: "1.01" });
 });
 
-app.use(function(req, res) {
+app.use(function (req, res) {
   res.status(404);
 });
 
-
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!`);
+});
+
+// Test route
+
+app.get("/api/users", (req, res) => {
+  const query = `SELECT * FROM users`;
+  console.log(query);
+  db.query(query)
+    .then((data) => {
+      const users = data.rows;
+      res.json({ users });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
 });
