fa8a57e78c692bd99af654c0acdb31e22b3e88be
Steve Willsmore <scwillsmore@gmail.com>
2023-02-06 19:51:02 -0500

    Refactor query.

--- server-express/db/queries/users.js
+++ server-express/db/queries/users.js
@@ -1,9 +1,24 @@
 const db = require("../connection");
 
+// Get all users
 const getUsers = () => {
   return db.query("SELECT * FROM users;").then((data) => {
     return data.rows;
   });
 };
 
-module.exports = { getUsers };
+// Get user by name
+const getUserByName = (username) => {
+  return db
+    .query(
+      `SELECT username
+        FROM users 
+        WHERE username = $1;`,
+      ["BigJim48"]
+    )
+    .then((data) => {
+      return data.rows;
+    });
+};
+
+module.exports = { getUsers, getUserByName };
