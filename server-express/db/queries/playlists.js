13e4d2753a485aca3fb4c77307aaa9c0eaeed0b6
Steve Willsmore <scwillsmore@gmail.com>
2023-02-07 17:26:06 -0500

    Refactor query to also grab related video info.

--- server-express/db/queries/playlists.js
+++ server-express/db/queries/playlists.js
@@ -2,7 +2,13 @@ const db = require("../connection");
 
 const getPlaylistsById = (id) => {
   return db
-    .query("SELECT * FROM playlists WHERE user_id = $1;", [id])
+    .query(
+      `SELECT videos.playlist_id, playlists.name, playlists.description, videos.yt_video_key, videos.title
+    FROM playlists
+    JOIN videos ON videos.playlist_id = playlists.id
+    WHERE playlists.user_id = $1;`,
+      [id]
+    )
     .then((data) => {
       return data.rows;
     });
