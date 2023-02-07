6636dc715c9f7e73e953f46741187d7caa05e082
Steve Willsmore <scwillsmore@gmail.com>
2023-02-06 19:51:38 -0500

    Refactor route.

--- server-express/routes/users-api.js
+++ server-express/routes/users-api.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const userQueries = require("../db/queries/users");
 
+// Get all users
 router.get("/", (req, res) => {
   userQueries
     .getUsers()
     .then((users) => {
       res.json({ users });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// Get user by name
+router.get("/name", (req, res) => {
+  userQueries
+    .getUserByName()
+    .then((user) => {
+      res.json({ user });
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
