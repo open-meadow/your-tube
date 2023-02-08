338a1279f02a2eef3d3440290c828c9d74fba0f4
wundeeh <jakiriksh@gmail.com>
2023-02-08 16:02:30 -0600

    add a put request to update the DB

--- server-express/routes/playlists-api.js
+++ server-express/routes/playlists-api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const playlistQueries = require("../db/queries/playlists");
+const { addToPlaylist } = require("../db/queries/addToPlaylist");
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
@@ -13,5 +14,20 @@ router.get("/:id", (req, res) => {
       res.status(500).json({ error: err.message });
     });
 });
+
+router.put("/:id", (req, res) => {
+  console.log("body====", req.body)
+  const plId = req.params.id
+  const vidId = req.body.vidId
+  const title = req.body.title
+  addToPlaylist(vidId, title, plId)
+    .then((data) => {
+      console.log("data====", data)
+      return res.send(data)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    })
+})
 
 module.exports = router;
