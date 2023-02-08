fd370a5ab7cfc5fc2aa70af2199e5e7d171b3f08
Steve Willsmore <scwillsmore@gmail.com>
2023-02-08 16:22:15 -0500

    Fix bug where empty playlists would not be returned.

--- server-express/db/queries/playlists.js
+++ server-express/db/queries/playlists.js
@@ -3,11 +3,12 @@ const db = require("../connection");
 const getPlaylistsById = (id) => {
   return db
     .query(
-      `SELECT videos.playlist_id, playlists.name AS playlist_name, playlists.description AS playlist_desc, json_agg(json_build_object(videos.yt_video_key, videos.title)) AS videos
+      `SELECT playlists.id AS playlist_id, playlists.name AS playlist_name, playlists.description AS playlist_desc,
+      COALESCE(json_agg(json_build_object(COALESCE(videos.yt_video_key, ''), COALESCE(videos.title, ''))), '[]') AS videos
       FROM playlists
-      JOIN videos ON videos.playlist_id = playlists.id
+      LEFT JOIN videos ON videos.playlist_id = playlists.id
       WHERE playlists.user_id = $1
-      GROUP BY videos.playlist_id, playlists.name, playlists.description`,
+      GROUP BY playlists.id, playlists.name, playlists.description;`,
       [id]
     )
     .then((data) => {
