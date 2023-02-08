4f5e2d132b32a8d4e2e1ffddcd73c320e82f6273
Steve Willsmore <scwillsmore@gmail.com>
2023-02-08 10:22:54 -0500

    Refactor to join videos table.

--- server-express/db/queries/playlists.js
+++ server-express/db/queries/playlists.js
@@ -3,7 +3,7 @@ const db = require("../connection");
 const getPlaylistsById = (id) => {
   return db
     .query(
-      `SELECT videos.playlist_id, playlists.name, playlists.description, videos.yt_video_key, videos.title
+      `SELECT videos.playlist_id, playlists.name as playlist_name, playlists.description as playlist_desc, videos.yt_video_key as video_key, videos.title as video_title
     FROM playlists
     JOIN videos ON videos.playlist_id = playlists.id
     WHERE playlists.user_id = $1;`,
