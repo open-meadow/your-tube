befc4d21b1aa17cbc4f14512b6ed228d76fc81eb
Steve Willsmore <scwillsmore@gmail.com>
2023-02-08 14:29:28 -0500

    Add video list functionality.

--- client-react/src/components/Playlist.js
+++ client-react/src/components/Playlist.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 export default function Playlist(props) {
-  console.log("plylst prps:", props);
+  const arrayOfVideos = props.videos;
+
+  // Map through the array of videos and return a li with the video title
+  const allTheVideos = arrayOfVideos.map((video) => {
+    return <li key={Object.keys(video)}>{Object.values(video)}</li>;
+  });
 
   return (
     <div>
       <h2>{props.playlist_name}</h2>
-      <ul>
-        <li>{props.video_title}</li>
-        {/* <li>Song 2</li>
-        <li>Song 3</li>
-        <li>Song 4</li> */}
-      </ul>
+      <p>{props.playlist_desc}</p>
+      <ul>{allTheVideos}</ul>
     </div>
   );
 }
