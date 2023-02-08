654e545ba39c2f948d44f7b60f2e26fc33bdfe34
Steve Willsmore <scwillsmore@gmail.com>
2023-02-08 13:41:18 -0500

    Refactor object structure output with psql json commands.

--- server-express/db/queries/playlists.js
+++ server-express/db/queries/playlists.js
@@ -3,10 +3,11 @@ const db = require("../connection");
 const getPlaylistsById = (id) => {
   return db
     .query(
-      `SELECT videos.playlist_id, playlists.name as playlist_name, playlists.description as playlist_desc, videos.yt_video_key as video_key, videos.title as video_title
-    FROM playlists
-    JOIN videos ON videos.playlist_id = playlists.id
-    WHERE playlists.user_id = $1;`,
+      `SELECT videos.playlist_id, playlists.name AS playlist_name, playlists.description AS playlist_desc, json_agg(json_build_object(videos.yt_video_key, videos.title)) AS videos
+      FROM playlists
+      JOIN videos ON videos.playlist_id = playlists.id
+      WHERE playlists.user_id = $1
+      GROUP BY videos.playlist_id, playlists.name, playlists.description`,
       [id]
     )
     .then((data) => {
