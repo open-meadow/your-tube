e01d1971d8c89b789e37ca6be55a62f57aac7504
open-meadow <rohan.varughese@protonmail.com>
2023-02-08 20:26:13 -0500

    In navigation, replace props to globalContext

--- client-react/src/components/Navigation.js
+++ client-react/src/components/Navigation.js
@@ -7,6 +7,7 @@ import Playlist from "./Playlist";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { useGlobalContext } from "context/context";
 
 export default function Nav(props) {
   const [show, setShow] = useState(false);
@@ -14,9 +15,10 @@ export default function Nav(props) {
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
 
-  // console.log("props:", props.playlists);
-
-  const arrayOfPlaylists = props.playlists;
+  // get username and playlists from global context
+  const { username, playlists } = useGlobalContext();
+  // made change here to use 'playlists' from global context instead of props.playlists
+  const arrayOfPlaylists = playlists;
 
   const allThePlaylists = arrayOfPlaylists.map((playlist) => {
     // console.log("props:", props.playlists);
@@ -38,7 +40,7 @@ export default function Nav(props) {
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end" id="user-name">
-        <Navbar.Text>{props.username}'s Playlists</Navbar.Text>
+        <Navbar.Text>{username}'s Playlists</Navbar.Text>
       </Navbar.Collapse>
       <Button className="open-sidebar" variant="dark" onClick={handleShow}>
         <FontAwesomeIcon icon={faArrowLeft} />
