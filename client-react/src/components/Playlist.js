9e52f59a11ef47fdad47eb93d14adde7dd4bb44b
open-meadow <rohan.varughese@protonmail.com>
2023-02-09 13:12:02 -0500

    Add thumbnail to playlist component

--- client-react/src/components/Playlist.js
+++ client-react/src/components/Playlist.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Playlist(props) {
+  // const [thumbnail, setThumbnail] = useState(null);
+
   const arrayOfVideos = props.videos;
 
   // Map through the array of videos and return a li with the video title
   const allTheVideos = arrayOfVideos.map((video) => {
-    console.log("here is video: ", video);
+    // console.log("video: ", video);
+    // fetch(`https://invidious.sethforprivacy.com/api/v1/videos/MWQkvbe5nyY`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("gotten from api: ", data);
+    //     setThumbnail(data.videoThumbnails[1].url);
+    //   });
+
+    const thumbnail = `https://inv.riverside.rocks/vi/${Object.keys(video)}/default.jpg`
+
     return (
-      <Link to={`/video/${Object.keys(video)}`}>
-        <li key={Object.keys(video)}>{Object.values(video)}</li>
+      <Link to={`/video/${Object.keys(video)}`} key={Object.keys(video)}>
+        <li>
+          <img src={thumbnail} />
+          {Object.values(video)}
+        </li>
       </Link>
     );
   });
