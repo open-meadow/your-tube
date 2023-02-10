4051a6c9252c120e3937fb54e6629750f418a799
Steve Willsmore <scwillsmore@gmail.com>
2023-02-09 20:33:48 -0500

    Add function to find and pop blank objects from video array.

--- client-react/src/components/Playlist.js
+++ client-react/src/components/Playlist.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import "./Playlist.css"
+import "./Playlist.css";
 
 export default function Playlist(props) {
   const arrayOfVideos = props.videos;
 
+  console.log(props);
+
+  const getRidOfBlankObject = (array) => {
+    for (let obj of array) {
+      // just look at the first item in video array
+      if (obj.hasOwnProperty("")) {
+        // get rid of it
+        array.pop();
+      }
+    }
+
+    return array;
+  };
+
+  getRidOfBlankObject(props.videos);
+
   // Map through the array of videos and return a li with the video title
   const allTheVideos = arrayOfVideos.map((video) => {
-    const thumbnail = `https://inv.riverside.rocks/vi/${Object.keys(video)}/default.jpg`
+    const thumbnail = `https://inv.riverside.rocks/vi/${Object.keys(
+      video
+    )}/default.jpg`;
 
     return (
       <Link to={`/video/${Object.keys(video)}`} key={Object.keys(video)}>
