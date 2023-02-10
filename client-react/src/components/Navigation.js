71beb9f51fa6f985fa539c0af8389159d0de8559
wundeeh <jakiriksh@gmail.com>
2023-02-10 15:26:43 -0600

    better comments

--- client-react/src/components/Navigation.js
+++ client-react/src/components/Navigation.js
@@ -9,7 +9,7 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 
 // Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faDesktop} from "@fortawesome/free-solid-svg-icons";
 
 // Other Imports
 import "./Navigation.css";
