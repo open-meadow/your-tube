a86ebb34eb57c1b732ced608e5cacc2c46a38c3c
Steve Willsmore <scwillsmore@gmail.com>
2023-02-10 13:36:44 -0500

    Update query.

--- server-express/db/queries/videos.js
+++ server-express/db/queries/videos.js
@@ -3,12 +3,7 @@ const db = require("../connection");
 // Get user by id
 const deleteVideoByKey = (key) => {
   return db
-    .query(
-      `SELECT *
-        FROM videos 
-        WHERE yt_video_key = $1;`,
-      [key]
-    )
+    .query(`DELETE FROM videos WHERE yt_video_key = $1::text;`, [key])
     .then((data) => {
       return data.rows;
     });
