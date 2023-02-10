188859277a488d5e226220da36aa206bdb079e77
Steve Willsmore <scwillsmore@gmail.com>
2023-02-10 17:18:53 -0500

    Change route for deleting videos.

--- server-express/routes/videos-api.js
+++ server-express/routes/videos-api.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const videoQueries = require("../db/queries/videos");
 
 // Delete video by key
-router.delete("/:key", (req, res) => {
-  const key = req.params.key;
+router.delete("/delete/", (req, res) => {
+  // const key = req.body.key[0];
+  // console.log(key);
   videoQueries
-    .deleteVideoByKey(key)
+    .deleteVideoByKey(req.body.key[0])
     .then(() => {
-      // res.json();
+      res.json();
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
