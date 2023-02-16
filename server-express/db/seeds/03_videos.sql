16228fa2bcb14b5b4913d2287d1986ea6182866a
open-meadow <rohan.varughese@protonmail.com>
2023-02-16 12:14:15 -0500

    Add new seed

--- server-express/db/seeds/03_videos.sql
+++ server-express/db/seeds/03_videos.sql
@@ -4,4 +4,5 @@ INSERT INTO videos (yt_video_key, title, playlist_id) VALUES
 ('h74pPIySz78', 'Night Sky - Lofi hip hop mix ~ Stress Relief, Relaxing Music', '2'),
 ('4ZUi5EP6rAI', 'Stop Overthinking 🔮 - Lofi hip hop mix - Calm Down And Relax - Lofi For Life', '2'),
 ('o4IQRX8zSJ4', 'Night In Japan Lofi Hip Hop ☯ - relaxing music to sleep/chill', '2'),
-('hQAHSlTtcmY', 'Learn React In 30 Minutes', '3');
+('hQAHSlTtcmY', 'Learn React In 30 Minutes', '3'),
+('Tn6-PIqc4UM', 'React in 100 Seconds', '3');
